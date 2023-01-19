ddda7c9bc75c0435a245fcf8e22d3ce93a2aa24b
SaLmAf3999 <farangizakayumova@gmail.com>
2023-01-19 17:10:59 +0400

    v_1_0

---
+++ printf/ft_printf.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fakayumo <fakayumo@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 17:00:04 by fakayumo          #+#    #+#             */
+/*   Updated: 2023/01/19 17:00:04 by fakayumo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdint.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_formats(va_list args, const char format);
+int		ft_printchar(int c);
+int		ft_printstr(char *str);
+int		ft_print_ptr(unsigned long long ptr);
+int		ft_printnbr(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int num, const char format);
+int		ft_printpercent(void);
+void	ft_putstr(char *str);
+char	*ft_itoa(int n);
+char	*ft_uitoa(unsigned int n);
+int		ft_num_len(unsigned	int num);
+void	ft_put_hex(unsigned int num, const char format);
+int		ft_hex_len(unsigned	int num);
+void	ft_putchar_fd(char c, int fd);
+#endif
