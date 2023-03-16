acb56a0456806d30b696b58cd1cf532499cf5c56
SaLmAf3999 <farangizakayumova@gmail.com>
2023-03-16 11:17:26 +0400

    v_1

---
+++ so_long/game_logic/controls.c
@@ -0,0 +1,134 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   controls.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fakayumo <fakayumo@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 11:38:23 by fakayumo          #+#    #+#             */
+/*   Updated: 2023/02/24 11:38:28 by fakayumo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static int	right_move(t_complete *game, int i, int j);
+
+int	movement_validate(t_complete *game, int i, int j, int movement)
+{
+	int	k;
+
+	if (movement == 1)
+	{
+		j++;
+		if (game->map[j][i] == '1')
+			return (0);
+		k = right_move(game, i, j);
+		if (!k)
+			return (0);
+		game->map[j - 1][i] = '0';
+	}
+	if (movement == 2)
+	{
+		i++;
+		if (game->map[j][i] == '1')
+			return (0);
+		k = right_move(game, i, j);
+		if (!k)
+			return (0);
+		game->map[j][i - 1] = '0';
+	}
+	printf("Steps Taken: %i\n", game->counter);
+	printf("Collectables Left: %i\n", game->collectables);
+	return (1);
+}
+
+static int	keyboard_a_d(t_complete *game, int movement)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = game->playerfirst;
+	j = game->playersecond;
+	if (movement == 0)
+	{
+		i--;
+		if (game->map[j][i] == '1')
+			return (0);
+		k = right_move(game, i, j);
+		if (!k)
+			return (0);
+		game->map[j][i + 1] = '0';
+	}
+	movement_validate(game, i, j, movement);
+	return (1);
+}
+
+static	int	keyboard_w_s(t_complete *game, int movement)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = game->playerfirst;
+	j = game->playersecond;
+	if (movement == 13)
+	{
+		j--;
+		if (game->map[j][i] == '1')
+			return (0);
+		k = right_move(game, i, j);
+		if (!k)
+			return (0);
+		game->map[j + 1][i] = '0';
+	}
+	movement_validate(game, i, j, movement);
+	return (1);
+}
+
+static int	right_move(t_complete *game, int i, int j)
+{
+	if (game->map[j][i] == 'E')
+	{
+		if (game->collectables != 0)
+			return (0);
+		printf("\e[31m\e[1m\nYou Have Won, Congrats! \e[0m \n");
+		exit_point(game);
+	}
+	if (game->map[j][i] == '0')
+	{
+		game->map[j][i] = 'P';
+		game->playerfirst = i;
+		game->playersecond = j;
+		game->counter++;
+	}
+	if (game->map[j][i] == 'C')
+	{
+		game->map[j][i] = 'P';
+		game->playerfirst = i;
+		game->playersecond = j;
+		game->collectables--;
+		game->counter++;
+	}
+	return (1);
+}
+
+int	controls_working(int command, t_complete *game)
+{
+	int	works;
+
+	if (command == 53)
+		exit_point(game);
+	if (command == 13)
+		works = keyboard_w_s(game, command);
+	if (command == 1)
+		works = keyboard_w_s(game, command);
+	if (command == 0)
+		works = keyboard_a_d(game, command);
+	if (command == 2)
+		works = keyboard_a_d(game, command);
+	if (works)
+		adding_in_graphics(game);
+	return (1);
+}
---
+++ so_long/headers/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fakayumo <fakayumo@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 11:39:20 by fakayumo          #+#    #+#             */
+/*   Updated: 2023/02/24 11:39:22 by fakayumo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int i);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
+
+#endif
